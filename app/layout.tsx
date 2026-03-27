9525e03041284fbbcccd3feb5f73038aaace9b84
Chandanmajhi7 <chandanmajhi9693@gmail.com>
2026-03-28 00:54:24 +0530

    updating social icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import FloatingSocialBar from "@/components/FloatingSocialBar";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
@@ -216,7 +217,7 @@ export default function RootLayout({
         />
 
         {children}
-
+<FloatingSocialBar/>
         <Toaster />
         <Analytics />
       </body>
