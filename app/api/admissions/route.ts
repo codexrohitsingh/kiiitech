e5dd314041c530a33fdd2c910733f43244587ce5
rohit singh <codex.rohit.singh@gmail.com>
2026-03-28 21:16:23 +0530

    new

--- app/api/admissions/route.ts
+++ app/api/admissions/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import nodemailer from "nodemailer";
-
+// hi
 // Configure Nodemailer
 const transporter = nodemailer.createTransport({
   service: "gmail",
