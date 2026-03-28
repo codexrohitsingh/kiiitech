e613c9a25980811016b7c64f10210d4658e26a2d
rohit singh <codex.rohit.singh@gmail.com>
2026-03-28 20:37:55 +0530

    env updated

--- app/api/admissions/route.ts
+++ app/api/admissions/route.ts
@@ -6,7 +6,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER || "rs21rohit@gmail.com",
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Use app password if 2FA enabled
   },
 });
